b7eaf74cff207d00e51fffcc9531a7d941064e54
Anurag Patil <aspanurag@gmail.com>
2020-03-06 14:37:08 -0500

    Login Frontend

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,9 +4,9 @@ import Login from "./Login";
 export default class App extends React.Component {
   render() {
     return (
-      <>
+      <div className="flex justify-center items-center h-screen">
         <Login></Login>
-      </>
+      </div>
     );
   }
 }
