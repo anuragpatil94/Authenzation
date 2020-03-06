cab9323b026499ecdad99f9ed55b7e8213925e98
Anurag Patil <aspanurag@gmail.com>
2020-03-06 05:40:53 -0500

    Login Component|Structure

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -2,7 +2,12 @@ import React from "react";
 
 class Login extends React.Component {
   render() {
-    return <></>;
+    return (
+      <div>
+        <form>
+        </form>
+      </div>
+    );
   }
 }
 
