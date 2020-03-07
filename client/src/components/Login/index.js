ee25ffd98a736162d445ef5618b3c249a72d2c56
Anurag Patil <aspanurag@gmail.com>
2020-03-07 11:43:45 -0500

    Separate Component for Login Form| Style Changes

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,58 +1,19 @@
 import React from "react";
+import LoginForm from "./LoginForm";
 
 class Login extends React.Component {
   render() {
     return (
-      <form className="bg-white shadow-md rounded px-8 py-6 ">
-        <div className="mb-4">
-          <label className="block text-gray-700 text-lg font-bold mb-2">
-            Username
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
-            placeholder="Username"
-          ></input>
+      <div className="flex flex-col justify-center items-center w-full">
+        <div className="flex w-1/3">
+          <div className="flex w-full">
+            <div>{/* Different Auth Options */}</div>
+            <div className="w-full md:max-w-sm rounded-lg">
+              <LoginForm></LoginForm>
+            </div>
+          </div>
         </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-lg font-bold mb-2">
-            Password
-          </label>
-          <input
-            type="password"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
-            placeholder="Password"
-          ></input>
-          {/* <p>Please choose a password</p> */}
-        </div>
-        <div className="mb-4">
-          <label>
-            <input type="checkbox"></input>
-            <span className="text-gray-700 pl-2">Remember me</span>
-          </label>
-        </div>
-        <div className="mb-4 flex items-center justify-between">
-          <button
-            className="bg-blue-500 w-1/2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2"
-            type="button"
-          >
-            Sign In
-          </button>
-          <button
-            className="bg-blue-500 w-1/2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ml-2"
-            type="button"
-          >
-            Sign Up
-          </button>
-        </div>
-        <div className="text-center">
-          <a
-            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-            href="/"
-          >
-            Forgot Password?
-          </a>
-        </div>
-      </form>
+      </div>
     );
   }
 }
