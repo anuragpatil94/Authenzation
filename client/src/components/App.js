d75925b350505170ca3a3ef1ddc404971d3cada2
Anurag Patil <aspanurag@gmail.com>
2020-03-07 05:11:36 -0500

    Added container for NavBar and Footer

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,9 +4,34 @@ import Login from "./Login";
 export default class App extends React.Component {
   render() {
     return (
-      <div className="flex justify-center items-center h-screen">
-        <Login></Login>
-      </div>
+      <>
+        <div className="flex flex-col items-center min-h-screen">
+          <header className="text-center bg-gray-800 text-gray-300 px-6 py-4 shadow-md w-full">
+            Navigation Bar
+            {/* TODO: Add a button here for dark/light theme */}
+            <div className="float-right">
+              <label className="switch">
+                <input type="checkbox" />
+                <span className="slider"></span>
+              </label>
+            </div>
+          </header>
+          <main className="flex flex-grow w-1/3">
+            <div className="flex flex-col justify-center items-center w-full">
+              <div className="bg-gray-400 w-32 h-32 mb-4 text-center text-gray-700">
+                Logo
+              </div>
+              <div>{/* Different Auth Options */}</div>
+              <div className="w-full md:max-w-md rounded-lg">
+                <Login></Login>
+              </div>
+            </div>
+          </main>
+          <footer className="text-center bg-gray-800 text-gray-300 px-6 py-4 shadow-md w-full">
+            Footer
+          </footer>
+        </div>
+      </>
     );
   }
 }
--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -3,58 +3,56 @@ import React from "react";
 class Login extends React.Component {
   render() {
     return (
-      <div className="w-full max-w-xs rounded-lg">
-        <form className="bg-white shadow-md rounded px-8 py-6 ">
-          <div className="mb-4">
-            <label className="block text-gray-700 text-lg font-bold mb-2">
-              Username
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
-              placeholder="Username"
-            ></input>
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-lg font-bold mb-2">
-              Password
-            </label>
-            <input
-              type="password"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
-              placeholder="Password"
-            ></input>
-            {/* <p>Please choose a password</p> */}
-          </div>
-          <div className="mb-4">
-            <label>
-              <input type="checkbox"></input>
-              <span className="text-gray-700 pl-2">Remember me</span>
-            </label>
-          </div>
-          <div className="mb-4 flex items-center justify-between">
-            <button
-              className="bg-blue-500 w-1/2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2"
-              type="button"
-            >
-              Sign In
-            </button>
-            <button
-              className="bg-blue-500 w-1/2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ml-2"
-              type="button"
-            >
-              Sign Up
-            </button>
-          </div>
-          <div className="text-center">
-            <a
-              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-              href="#"
-            >
-              Forgot Password?
-            </a>
-          </div>
-        </form>
-      </div>
+      <form className="bg-white shadow-md rounded px-8 py-6 ">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-lg font-bold mb-2">
+            Username
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
+            placeholder="Username"
+          ></input>
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-lg font-bold mb-2">
+            Password
+          </label>
+          <input
+            type="password"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
+            placeholder="Password"
+          ></input>
+          {/* <p>Please choose a password</p> */}
+        </div>
+        <div className="mb-4">
+          <label>
+            <input type="checkbox"></input>
+            <span className="text-gray-700 pl-2">Remember me</span>
+          </label>
+        </div>
+        <div className="mb-4 flex items-center justify-between">
+          <button
+            className="bg-blue-500 w-1/2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2"
+            type="button"
+          >
+            Sign In
+          </button>
+          <button
+            className="bg-blue-500 w-1/2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ml-2"
+            type="button"
+          >
+            Sign Up
+          </button>
+        </div>
+        <div className="text-center">
+          <a
+            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+            href="/"
+          >
+            Forgot Password?
+          </a>
+        </div>
+      </form>
     );
   }
 }
