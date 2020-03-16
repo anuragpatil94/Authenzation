91f711366511466fef6cf00feb610e591e3895c5
Anurag Patil <aspanurag@gmail.com>
2020-03-15 22:30:25 -0400

    Route Error Handlers

--- server/auth/src/middleware/routes.js
+++ server/auth/src/middleware/routes.js
@@ -1,9 +1,34 @@
+import configs from "../config";
+import routes from "../routes";
+
 export default ({ app }) => {
-  // TODO: Connect to Routes
-  app.get("/favicon.ico", (req, res) => res.status(204).end());
-  app.get("/", (req, res, next) => {
-    res.status(200).end();
+  app.use(configs.routesConfig.routePrefix, routes());
+
+  // catch 404 and forward to error handler
+  app.use((req, res, next) => {
+    const err = new Error("Not Found");
+    err["status"] = 404;
+    next(err);
   });
 
-  //TODO: Error Handling Routes
+  app.use((err, req, res, next) => {
+    /**
+     * Handle 401 thrown by jwt
+     */
+    if (err.name === "UnauthorizedError") {
+      return res
+        .status(err.status)
+        .send({ message: err.message })
+        .end();
+    }
+    return next(err);
+  });
+
+  app.use((err, req, res, next) => {
+    res.sendStatus(err.status || 500).json({
+      errors: {
+        message: err.message
+      }
+    });
+  });
 };
