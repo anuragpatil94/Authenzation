bba6620ee5202be00324e72c87367814f3536c58
Anurag Patil <aspanurag@gmail.com>
2020-03-17 12:53:01 -0400

    Added Schema to Users Collection and Index with username as key

--- server/auth/src/middleware/database.js
+++ server/auth/src/middleware/database.js
@@ -4,7 +4,65 @@ import config from "../config";
 import { Logger } from "./logger";
 import { constants } from "../util";
 
-export default async () => {
+const usersSchema = {
+  $jsonSchema: {
+    bsonType: "object",
+    required: [
+      "id",
+      "details",
+      "username",
+      "password",
+      "salt",
+      "userLevel",
+      "createdAt"
+    ],
+    properties: {
+      details: {
+        bsonType: "object",
+        required: ["firstName", "lastName"],
+        properties: {
+          firstName: {
+            bsonType: "string"
+          },
+          middleName: {
+            bsonType: "string"
+          },
+          lastName: {
+            bsonType: "string"
+          },
+          age: {
+            bsonType: "int"
+          },
+          addressId: {
+            bsonType: "objectId"
+          }
+        }
+      },
+      username: {
+        bsonType: "string",
+        description:
+          "Username is a Unique string which is also an Index and is Required"
+      },
+      password: {
+        bsonType: "string",
+        description: "BCrypted Password with Salt"
+      },
+      salt: {
+        bsonType: "string",
+        description: "Salt used to Hash Password"
+      },
+      userLevel: {
+        enum: ["admin", "general"],
+        description: "This is based on level of access granted to the user"
+      },
+      createdAt: {
+        bsonType: "date"
+      }
+    }
+  }
+};
+
+export const databaseConnection = async () => {
   try {
     const url = config.databaseConfig.mongoURI;
     const username = config.databaseConfig.mongoUser;
@@ -21,12 +79,37 @@ export default async () => {
     await mongoClient.connect();
     const db = mongoClient.db(databaseName);
 
-    if (!db) {
-      throw new Error("Cannot connect to database");
+    const collections = await db
+      .listCollections({}, { nameOnly: true })
+      .toArray();
+
+    const collectionList = collections.map(collection => collection.name);
+
+    // UNCOMMENT IF YOU WANT TO DELETE USERS COLLECTION
+    // if (process.env.NODE_ENV === "development") {
+    //   if (collectionList.includes("users")) {
+    //     await db.dropCollection("users");
+    //     collectionList.pop("users");
+    //     Logger.info("Dropped Users Collection");
+    //   }
+    // }
+
+    if (!collectionList.includes("users")) {
+      await (
+        await db.createCollection("users", {
+          validator: usersSchema,
+          validationLevel: "strict",
+          validationAction: "error"
+        })
+      ).createIndex({ username: 1 }, { name: "userNameIndex", unique: true });
+
+      Logger.info("Created Users Collection");
     }
-    
+
     return db;
   } catch (err) {
+    Logger.error("Connection to database failed!!");
     Logger.error(err);
+    return 0;
   }
 };
